ddec2affbd6c3a0aad350bb19936cd3d923bdb33
Matestampa <paredpiso5@gmail.com>
2024-07-04 03:03:02 -0300

    added new utils

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 const {APP_VARS}=require("./config.js");
 
-const {JsonFile_Access,sleep}=require("./utils.js");
+const {JsonFile_Access,getOrdered_AccountsArray,
+       Time_Calculator,sleep}=require("./utils.js");
 
 const {userInfo_igRequest}=require("./req_userInfo.js");
 const {followers_igRequest}=require("./followers_request.js");
 
 
-const TEST_USERNAME="matestamparin";
+const TEST_USERNAME="paloweigel";
 const TEST_USER_ID=3407050365; //(matestamparin)
 
-async function test_AllProcess(){
-    const JsonAccess=new JsonFile_Access(APP_VARS.igAccounts_file);
+const MS_BTW_REQ=400;
+const MS_BTW_REQGROUP=2000;
 
-    let ACCOUNTS_DATA=JsonAccess.read();
-    console.log(Object.keys(ACCOUNTS_DATA));
+async function test_AllProcess(ACCOUNTS_DATA,AccountsUse_Manager,Time_Calc){
 
-    let AccountsUse_Manager=create_AccountsUse_Manager(ACCOUNTS_DATA);
-    
     let accountKey;
-    
     //----------------- Traer user info -----------------------------------------------
     //Cuenta para hacer la request.
     accountKey=await AccountsUse_Manager.get_availAccount();
@@ -39,9 +36,9 @@ async function test_AllProcess(){
                                      accountAuth.headers,accountProxy.url);
     }
     catch(e){
-        console.log(error);
+        console.log(e);
         console.log("Account fail:",accountKey);
-        return;
+        return accountKey;
     }
     
     console.log(user_info);
@@ -52,6 +49,7 @@ async function test_AllProcess(){
     console.log("STARTING FOLLOWERS");
     let followers={};
     let cursor;
+    let req_cont=1;
     
     //Hacemos las requests para traer de a poco los followers
     do{ 
@@ -60,6 +58,7 @@ async function test_AllProcess(){
         accountKey=await AccountsUse_Manager.get_availAccount();
         
         console.log(`Cuenta actual:${accountKey}`);
+        console.log(`Req actual: ${req_cont}`);
 
         //Si ya no hay cuentas disponibles
         if (!accountKey){
@@ -73,14 +72,16 @@ async function test_AllProcess(){
         let data={};
         
         try{
+            Time_Calc.start()
             data=await followers_igRequest(user_id,cursor,accountAuth.cookies,accountProxy.url);
+            Time_Calc.finish();
         } 
         
         //Ver si tiro errror la req
         catch(error){
             console.log(error);
             console.log("Account fail:",accountKey);
-            return;
+            break;
         }
         
         //Si no, vamos agregando los followers.
@@ -94,17 +95,13 @@ async function test_AllProcess(){
         
         //Ponemos un rate-limiting
         if (req_cont%3==0){ //si llego a 3 req seguidas
-            await sleep(randNum(2000,2300))
+            await sleep(randNum(MS_BTW_REQGROUP,MS_BTW_REQGROUP+1200))
         }
         else{ //el resto
-           await sleep(randNum(1000,1300));
+           await sleep(randNum(MS_BTW_REQ,MS_BTW_REQ+200));
         }
-
-        /*Chequear limit de request para test
-        req_cont++;
-        if (req_cont>=cantReq){
-            break
-        }*/
+        req_cont++
+        
     }
     
     //Mientras el cursor siga teniendo contenido(es decir q todavia falten por traer)
@@ -113,21 +110,22 @@ async function test_AllProcess(){
     let totalFollowers=Object.keys(followers).length;
     console.log("Cant followers:",totalFollowers);
 
+    return accountKey;
+
 }
 
 
 //------------------------- UTILS -------------------------------------------
 
-function create_AccountsUse_Manager(accounts_data){
+function create_AccountsUse_Manager(accountsArr,accounts_data){
     let accounts_keys=[];
     
     //Solo metemos las keys de las account q esten activas
-    for (let key of Object.keys(accounts_data)){
+    for (let key of accountsArr){
         if (accounts_data[key].active){
             accounts_keys.push(key);
         }
     }
-
     return new IgAccountsUse_Manager(accounts_keys);
 }
 
@@ -210,4 +208,31 @@ function randNum(min,max){
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-test_AllProcess();
+
+async function dale(){
+    //leer archivo de ultima cuenta usada
+    let lastAccJson=new JsonFile_Access(APP_VARS.lastAccount_file);
+    let lastAcc=lastAccJson.read().account;
+    
+
+    //leer archivo de data de cuentas
+    let AccDataJson=new JsonFile_Access(APP_VARS.igAccounts_file);
+    let ACCOUNTS_DATA=AccDataJson.read();
+
+    //hacer array ordenado
+    let orderedArray=getOrdered_AccountsArray(Object.keys(ACCOUNTS_DATA),lastAcc);
+
+    //Crear account use manager
+    let AccountsUse_Manager=create_AccountsUse_Manager(orderedArray,ACCOUNTS_DATA);
+
+    //llamar a la func con (data de cuentas,accounts use manager)
+    let TimeCalculator=new Time_Calculator();
+    lastAcc=await test_AllProcess(ACCOUNTS_DATA,AccountsUse_Manager,TimeCalculator);
+    
+    console.log(TimeCalculator.get_summary());
+
+    //escribir ultima cuenta usada
+    lastAccJson.write({"account":lastAcc})
+}
+
+dale();
--- src/utils.js
+++ src/utils.js
@@ -28,9 +28,58 @@ class JsonFile_Access{
     }
 }
 
+function getOrdered_AccountsArray(array, value) {
+    let resultArray = [];
+    let startIndex = array.indexOf(value)+1;
+    
+    if (startIndex==array.length){
+        startIndex=0;
+    }
+
+    if (startIndex === -1) {
+        // El valor no se encontró en el array
+        return resultArray;
+    }
+
+    // Concatenar los valores desde el valor encontrado hasta el final del array
+    resultArray = array.slice(startIndex);
+    
+    // Concatenar los valores desde el principio del array hasta justo antes del valor encontrado
+    resultArray = resultArray.concat(array.slice(0, startIndex));
+
+    return resultArray;
+}
+
+class Time_Calculator{
+    constructor(){
+        this.total_seconds=0;
+        this.actual_time;
+    }
+
+    start(){
+        this.start_time=new Date();
+    };
+
+    finish(){
+        //CALCULAR DIFERENCIA.
+        let time_difference=new Date() - this.start_time;
+        let time_difference_secs=time_difference/1000
+        this.total_seconds+=(time_difference_secs);
+
+        console.log(`Time difference:${time_difference_secs}`);
+
+
+    };
+
+    get_summary(){
+        return this.total_seconds;
+    };
+
+}
+
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
-module.exports={JsonFile_Access,sleep};
+module.exports={JsonFile_Access,getOrdered_AccountsArray,Time_Calculator,sleep};
